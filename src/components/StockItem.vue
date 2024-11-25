4e379cd57adaaea0fa670267b2415330748255ff
Oskar Engström <oskar.fv57@gmail.com>
2024-11-25 16:53:53 +0100

    feat(Stock): added stock item

---
+++ src/components/StockItem.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <p>{{item.name}}</p>
+    <p>{{item.location}}</p>
+    <p>{{item.current}}</p>
+    <input v-model.number="input" />
+    <p :class="diffClass">{{input - item.current}}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+
+const props = defineProps({
+  item: {
+    name: String,
+    current: Number,
+  },
+});
+
+const input = ref(props.item.current)
+
+
+const diffClass = computed(() => {
+  const diff = input.value - props.item.current
+  if (diff >= 0) {
+    return "Green"
+  } else {
+    return "Red"
+  }
+})
+
+</script>
+
+<style scoped>
+  * {
+    font-family: Lato;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  }
+
+  p,a {
+    border-right: 1px solid lightgray;
+    padding: 1rem;
+  }
+
+  .Red {
+    color: red;
+  }
+
+  .Green {
+    color: green;
+  }
+
+  input {
+    width: 60%;
+  }
+
+  button {
+    padding: 1rem;
+  }
+</style>
--- src/views/TakeStock.vue
+++ src/views/TakeStock.vue
@@ -1,37 +1,58 @@
 <template>
-  <div>
+  <div class="main">
+    <div class="header">
+      <p>Produkt</p>
+      <p>Plats</p>
+      <p>Nuvarande</p>
+      <p>Nya</p>
+      <p>Differens</p>
+    </div>
     <form v-on:submit.prevent="updateItems">
-      <li v-for="item in items" :key="item.name">
-        {{ item.name }} {{ item.current }} <input v-model.number="input[item.name]" /> {{ item.current - input[item.name] }}
-      </li>
-      <input type="submit" />
+      <div v-for="item in items" :key="item.name">
+        <StockItem :item="item" />
+      </div>
+      <input type="submit" value="Inventera" />
     </form>
   </div>
-  <button></button>
 </template>
 
 <script setup lang="ts">
-  import {ref, reactive} from 'vue'
-  const items = ref(null)
-  // Probebly not a good idea to use an object, due to security, should probebly use map
-  const input = reactive({})
-  const HOST = import.meta.env.VITE_HOST;
+import StockItem from '@/components/StockItem.vue';
+import { ref, reactive } from 'vue'
+const items = ref(null)
+// Probebly not a good idea to use an object, due to security, should probebly use map
+const input = reactive({})
+const HOST = import.meta.env.VITE_HOST;
 
-  fetch(HOST + "/api/metadorerna/items").then((res) => res.json()).then((json) => {
-    items.value = json
-    json.forEach(item => {
-      input[item.name] = 0
-    });
+fetch(HOST + "/api/metadorerna/items").then((res) => res.json()).then((json) => {
+  items.value = json
+  json.forEach(item => {
+    input[item.name] = 0
+  });
+})
+
+const updateItems = async () => {
+  await fetch(HOST + "/api/metadorerna/take_stock", {
+    method: "POST",
+    body: JSON.stringify(Object.entries(input)),
   })
-
-  const updateItems = async () => {
-    await fetch(HOST + "/api/metadorerna/take_stock", {
-      method: "POST",
-      body: JSON.stringify(Object.entries(input)),
-    })
-  }
+}
 </script>
 
 <style scoped>
+.main {
+  max-width: 1024px;
+  margin: 1rem auto;
+}
 
+.header {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+}
+
+.header p,
+a {
+  border-right: 1px solid lightgray;
+  padding: 1rem;
+}
 </style>
