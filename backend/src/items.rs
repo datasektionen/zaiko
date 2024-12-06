f39b3bcdb8f13aa4069ae95b7ca724c0c24ca6c8
WALL-E <wall-e.axiom@proton.me>
2024-12-06 14:23:08 +0100

    feat: add tests for items

--- backend/src/items.rs
+++ backend/src/items.rs
@@ -98,3 +98,91 @@ pub(crate) async fn get_items(club: web::Path<String>, pool: web::Data<Pool<Sqli
         Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use actix_web::{test, web, App};
+    use sqlx::SqlitePool;
+
+    use super::{add_item, get_items, update_item, AddItem};
+
+    #[actix_web::test]
+    async fn test_get_all_items() {
+        let pool = web::Data::new(
+            SqlitePool::connect("db.sqlite")
+                .await
+                .expect("Expected sqlite database with name db.sqlite"),
+        );
+
+        let app = test::init_service(App::new().app_data(pool).service(get_items)).await;
+        let req = test::TestRequest::get().uri("/metadorerna/items").to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_success());
+    }
+
+    #[actix_web::test]
+    async fn test_add_item() {
+        let pool = web::Data::new(
+            SqlitePool::connect("db.sqlite")
+                .await
+                .expect("Expected sqlite database with name db.sqlite"),
+        );
+
+        let body = AddItem {
+            name: String::from("tejp"),
+            min: Some(10.0),
+            max: Some(20.0),
+            current: 15.0,
+            location: String::from("Unknown"),
+            supplier: None,
+            link: None,
+        };
+
+        let app = test::init_service(App::new().app_data(pool).service(add_item)).await;
+        let req = test::TestRequest::post().uri("/metadorerna/item").set_json(body).to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_success());
+    }
+
+    #[actix_web::test]
+    async fn test_update_item() {
+        let pool = web::Data::new(
+            SqlitePool::connect("db.sqlite")
+                .await
+                .expect("Expected sqlite database with name db.sqlite"),
+        );
+
+        let mut body = AddItem {
+            name: String::from("tejp"),
+            min: Some(10.0),
+            max: Some(20.0),
+            current: 15.0,
+            location: String::from("Unknown"),
+            supplier: None,
+            link: None,
+        };
+
+        let club = String::from("metadorerna");
+
+        sqlx::query!(
+            "UPDATE items SET location = $1, min = $2, max = $3, current = $4, supplier = $5, updated = strftime('%s', 'now'), link = $6  WHERE name = $7 AND club = $8",
+            body.location,
+            body.min,
+            body.max,
+            body.current,
+            body.supplier,
+            body.link,
+            body.name,
+            club,
+        )
+        .execute(pool.get_ref())
+        .await.unwrap();
+
+        body.location = String::from("Metador closet");
+
+        let app = test::init_service(App::new().app_data(pool).service(update_item)).await;
+        let req = test::TestRequest::post().uri("/metadorerna/update").set_json(body).to_request();
+        let res = test::call_service(&app, req).await;
+        assert!(res.status().is_success());
+    }
+}
