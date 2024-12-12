91a68d5a63ef00e06d614ff1342e99038e1d462b
Oskar Engström <oskar.fv57@gmail.com>
2024-12-12 18:39:43 +0100

    feat(add_supplier): Add Supplier

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AddItem from '../views/AddItem.vue'
 import TakeStock from '../views/TakeStock.vue';
 import MainPage from '../views/MainPage.vue';
+import AddSupplier from '@/views/AddSupplier.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,7 +21,12 @@ const router = createRouter({
       path: '/stock',
       name: 'take stock',
       component: TakeStock
-    }
+    },
+    {
+      path: '/addSupplier',
+      name: 'add supplier',
+      component: AddSupplier,
+    },
   ],
 })
 
