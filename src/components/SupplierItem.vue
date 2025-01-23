06a7fea00b0ec4fcbe5fe0d7ee8ad9b10e5bce5c
Oskar <70148221+TXTstyle@users.noreply.github.com>
2025-01-23 17:50:34 +0100

    Supplier pages
    * feat(add_supplier): Add Supplier
    
    * feat(Suppliers): List all suppliers
    
    * Methone fixed
    
    * fixes fixes methone

---
+++ src/components/SupplierItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>{{item.name}}</p>
+    <p>{{item.username}}</p>
+    <p>{{item.password}}</p>
+    <a :href="item.link">Link</a>
+    <p>{{item.note}}</p>
+    <button>Edit</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  item: {
+    name: String,
+    username: String,
+    password: String,
+    link: String,
+    note: String,
+  },
+});
+
+</script>
+
+<style scoped>
+  * {
+    font-family: Lato;
+  }
+
+  div {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr 2fr 1fr;
+  }
+
+  p,a {
+    border-right: 1px solid lightgray;
+    padding: 1rem;
+  }
+
+  button {
+    padding: 1rem;
+  }
+</style>
