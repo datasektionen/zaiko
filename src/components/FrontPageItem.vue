b397bb7c9204db4b587e97cb7f8291a25e1bbde2
Oskar Engström <oskar.fv57@gmail.com>
2025-01-27 21:48:46 +0100

    style changes done

--- src/components/FrontPageItem.vue
+++ src/components/FrontPageItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a :href="item.link" v-if="item.link">{{ item.name }}</a>
+    <a target="_blank" :href="item.link" v-if="item.link">{{ item.name }}</a>
     <p v-else>{{ item.name }}</p>
     <p>{{ item.location }}</p>
     <p>{{ item.current }}</p>
@@ -41,6 +41,7 @@ div {
   grid-template-columns: 1fr 1fr 1fr 1fr;
   border-bottom: 2px solid rgba(0, 105, 92, 0.25);
   padding: 10px 0;
+  background-color: rgba(0,0,0,0);
 }
 
 p, a {
