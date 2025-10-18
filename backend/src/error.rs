c86e8d1dbc25e655e6e4412485ec5dcfcdd986a3
Viktor Ekby <viktoe@datasektionen.se>
2025-10-18 14:28:39 +0200

    ref: improve error logging

--- backend/src/error.rs
+++ backend/src/error.rs
@@ -1,40 +1,15 @@
-use std::{
-    env::VarError,
-    fmt::{Display, Formatter},
-};
+use std::env::VarError;
 
 use actix_web::{http::StatusCode, HttpResponse, ResponseError};
 use derive_more::Display;
 use openidconnect::{
-    core::CoreErrorResponseType, reqwest, ClaimsVerificationError,
-    ConfigurationError, HttpClientError, RequestTokenError,
-    SignatureVerificationError, SigningError, StandardErrorResponse,
+    core::CoreErrorResponseType, reqwest, ClaimsVerificationError, ConfigurationError,
+    HttpClientError, RequestTokenError, SignatureVerificationError, SigningError,
+    StandardErrorResponse,
 };
+use utoipa::{ToResponse, ToSchema};
 
-use crate::auth::types::Permission;
-
-#[derive(Debug)]
-pub struct PermissionDifference {
-    permission_name: String,
-    permission_type: Permission,
-    required_permission_name: String,
-    required_permission_type: Permission,
-}
-
-impl Display for PermissionDifference {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(
-            f,
-            "tried to access {} {} resource using {} {} privalge",
-            self.required_permission_name,
-            self.required_permission_type,
-            self.permission_name,
-            self.permission_type
-        )
-    }
-}
-
-#[derive(Debug, Display)]
+#[derive(Debug, Display, ToResponse, ToSchema)]
 pub enum Error {
     InternalServerError(String),
     Unauthorized,
@@ -43,11 +18,6 @@ pub enum Error {
 
 impl ResponseError for Error {
     fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
-        match self {
-            Error::InternalServerError(error) => log::error!("{}", error),
-            _ => {}
-        }
-
         HttpResponse::build(self.status_code()).body(self.to_string())
     }
 
@@ -60,52 +30,66 @@ impl ResponseError for Error {
     }
 }
 
+impl From<sqlx::types::uuid::Error> for Error {
+    fn from(value: sqlx::types::uuid::Error) -> Self {
+        log::error!("{}", value);
+        Error::BadRequest
+    }
+}
+
 impl From<sqlx::Error> for Error {
-    fn from(_value: sqlx::Error) -> Self {
-        // TODO: FIX
-        Error::InternalServerError(String::from("sql"))
+    fn from(value: sqlx::Error) -> Self {
+        log::error!("{}", value);
+        Error::InternalServerError(format!("sql: {}", value))
     }
 }
 
 impl From<serde_json::Error> for Error {
-    fn from(_value: serde_json::Error) -> Self {
+    fn from(value: serde_json::Error) -> Self {
+        log::error!("{}", value);
         Error::BadRequest
     }
 }
 
 impl From<ConfigurationError> for Error {
     fn from(value: ConfigurationError) -> Self {
+        log::error!("{}", value);
         Error::InternalServerError(format!("token clinet request config error: {}", value))
     }
 }
 
 impl From<ClaimsVerificationError> for Error {
     fn from(value: ClaimsVerificationError) -> Self {
+        log::error!("{}", value);
         Error::InternalServerError(format!("aquireing claims error: {}", value))
     }
 }
 
 impl From<SignatureVerificationError> for Error {
     fn from(value: SignatureVerificationError) -> Self {
+        log::error!("{}", value);
         Error::InternalServerError(format!("aquireing signing algorithm: {}", value))
     }
 }
 
 impl From<SigningError> for Error {
     fn from(value: SigningError) -> Self {
+        log::error!("{}", value);
         Error::InternalServerError(format!("checking hash error: {}", value))
     }
 }
 
 impl From<VarError> for Error {
     fn from(value: VarError) -> Self {
-        Error::InternalServerError(format!("getting pls url error: {}", value))
+        log::error!("{}", value);
+        Error::InternalServerError(format!("env var error: {}", value))
     }
 }
 
 impl From<reqwest::Error> for Error {
     fn from(value: reqwest::Error) -> Self {
-        Error::InternalServerError(format!("failed to get data from pls: {}", value))
+        log::error!("{}", value);
+        Error::InternalServerError(format!("request error: {}", value))
     }
 }
 
@@ -117,6 +101,7 @@ where
     fn from(
         value: RequestTokenError<HttpClientError<ER>, StandardErrorResponse<CoreErrorResponseType>>,
     ) -> Self {
+        log::error!("{}", value);
         Error::InternalServerError(format!("token request error: {}", value))
     }
 }
