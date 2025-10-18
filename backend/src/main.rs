640517c5fb540e298918aa4ed654cb123f1e7dcf
Viktor Ekby <viktoe@datasektionen.se>
2025-10-18 14:30:14 +0200

    feat: add automatic api docs

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,33 +1,35 @@
-use std::env;
-
+#[warn(clippy::pedantic)]
 use actix_cors::Cors;
 use actix_web::{
     http::Method,
     middleware::Logger,
-    web::{self, scope, Data},
+    web::{self, Data},
     App, HttpServer,
 };
-use auth::types::{AuthMiddleware, Permission};
-use auth::{auth_callback, get_clubs, set_club, types::OIDCData};
 use dotenv::dotenv;
-use serve::serve_frontend;
-use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
-use stats::get_stats;
-use supplier::get_suppliers;
+use std::env;
+use utoipa::OpenApi;
+use utoipa_actix_web::{scope, AppExt};
+use utoipa_redoc::{Redoc, Servable};
 
 mod auth;
+mod db;
 mod error;
-mod serve;
 mod item;
-mod loging;
+mod logging;
+mod serve;
+mod shipment;
 mod shortage;
 mod stats;
+mod storage;
 mod supplier;
 
-use crate::item::{add_item, delete_item, get_item, update_item};
-use crate::loging::get_log;
-use crate::shortage::{get_shortage, take_stock};
-use crate::supplier::{add_supplier, delete_supplier, get_supplier, update_supplier};
+use auth::types::{AuthMiddleware, OIDCData};
+use db::init_db;
+use serve::serve_frontend;
+
+#[derive(OpenApi)]
+struct ApiDoc;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -36,14 +38,12 @@ async fn main() -> std::io::Result<()> {
         let _ = dotenv();
     }
 
-    let pool = web::Data::new(
-        PgPoolOptions::new()
-            .connect(&env::var("DATABASE_URL").expect("DATABASE_URL to exist"))
-            .await
-            .expect("Expected to connect to database"),
-    );
+    let pool = web::Data::new(init_db().await);
 
-    db_init(&pool).await.expect("to setup db");
+    sqlx::migrate!("./migrations")
+        .run(pool.get_ref())
+        .await
+        .expect("migrations to run");
 
     let (oidc, auth_path) = OIDCData::get_oidc().await;
     let oidc = Data::new(oidc);
@@ -58,42 +58,40 @@ async fn main() -> std::io::Result<()> {
                     Method::GET,
                     Method::POST,
                     Method::PATCH,
+                    Method::PUT,
                     Method::DELETE,
                 ])
                 .allow_any_header()
         };
 
         App::new()
-            .wrap(cors)
-            .wrap(Logger::default())
-            .wrap(AuthMiddleware::new(auth_url.clone(), Permission::Read))
-            .app_data(pool.clone())
-            .app_data(oidc.clone())
-            .service(auth_callback)
+            .into_utoipa_app()
+            .openapi(ApiDoc::openapi())
+            .map(|app| {
+                app.wrap(cors)
+                    .wrap(Logger::default())
+                    .app_data(pool.clone())
+                    .app_data(oidc.clone())
+            })
+            .service(utoipa_actix_web::scope("/auth").configure(auth::config()))
             .service(
-                scope("/api")
-                    .service(get_item)
-                    .service(get_shortage)
-                    .service(get_log)
-                    .service(get_stats)
-                    .service(get_clubs)
-                    .service(set_club)
-                    .service(
-                        scope("/admin")
-                            .wrap(AuthMiddleware::new(auth_url.clone(), Permission::ReadWrite))
-                            .service(get_supplier)
-                            .service(get_suppliers)
-                            .service(add_item)
-                            .service(update_item)
-                            .service(delete_item)
-                            .service(add_supplier)
-                            .service(update_supplier)
-                            .service(delete_supplier)
-                            .service(take_stock),
-                    ),
+                scope("/api") // Every thing in this scope requires the user to be logged in using sso
+                    .wrap(AuthMiddleware::new(auth_url.clone()))
+                    .configure(item::config())
+                    .configure(supplier::config())
+                    .configure(storage::config())
+                    .configure(shortage::config())
+                    .configure(stats::config())
+                    .configure(logging::config())
+                    .configure(shipment::config())
+                    .service(auth::user_info),
             )
-            .service(serve_frontend)
-            .service(actix_files::Files::new("/", "../dist/").index_file("index.html"))
+            .service(scope("/").map(|app| {
+                app.service(serve_frontend)
+                    .service(actix_files::Files::new("/", "../dist/").index_file("index.html"))
+            }))
+            .openapi_service(|api| Redoc::with_url("/docs/api", api))
+            .into_app()
     })
     .bind((
         env::var("APP_URL").expect("APP_URL in .env"),
@@ -104,49 +102,4 @@ async fn main() -> std::io::Result<()> {
     ))?
     .run()
     .await
-}
-
-async fn db_init(pool: &web::Data<Pool<Postgres>>) -> Result<(), sqlx::error::Error> {
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS items(
-        id SERIAL PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL,
-        location TEXT NOT NULL,
-        min REAL,
-        max REAL,
-        current REAL NOT NULL,
-        supplier INTEGER,
-        updated INTEGER NOT NULL,
-        link TEXT,
-        club TEXT NOT NULL);",
-    )
-    .execute(pool.get_ref())
-    .await?;
-
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS suppliers(
-        id SERIAL PRIMARY KEY NOT NULL,
-        name TEXT NOT NULL,
-        link TEXT,
-        notes TEXT,
-        username TEXT,
-        password TEXT,
-        updated INTEGER NOT NULL,
-        club TEXT NOT NULL);"
-    )
-    .execute(pool.get_ref())
-    .await?;
-
-    sqlx::query!(
-        "CREATE TABLE IF NOT EXISTS log(
-        id SERIAL PRIMARY KEY NOT NULL,
-        item_id INTEGER NOT NULL,
-        amount REAL NOT NULL,
-        time INTEGER NOT NULL,
-        club TEXT NOT NULL);"
-    )
-    .execute(pool.get_ref())
-    .await?;
-
-    Ok(())
 }
