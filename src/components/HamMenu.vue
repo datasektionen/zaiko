46906faee644d5400a8acc9cca5c561944331ffb
Oskar Engström <oskar.fv57@gmail.com>
2025-10-18 20:34:18 +0200

    fix: css overflow

--- src/components/HamMenu.vue
+++ src/components/HamMenu.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <div class="relative inline-block">
-      <EllipsisVerticalIcon class="h-6 w-6 text-(--zaiko-text) cursor-pointer" @click="isOpen = !isOpen"/>
-      <div v-if="isOpen" v-on-click-outside="() => isOpen = !isOpen" class="absolute top-0 right-5 bg-(--zaiko-bg-1) border border-(--zaiko-bg-2) rounded-md shadow-md w-fit min-w-36 z-10">
-        <ul class="flex flex-col gap-2">
-          <li class="p-2 hover:bg-(--zaiko-bg-2) hover:cursor-pointer" v-for="row in Object.keys(rows)" @click="click(row)" :key="row">
-            {{ rows[row] }}
-          </li>
-        </ul>
+      <EllipsisVerticalIcon class="h-6 w-6 text-(--zaiko-text) cursor-pointer" @click="isOpen = !isOpen" />
+      <div class="fixed z-10 bg-(--zaiko-bg-1)">
+        <div v-if="isOpen" v-on-click-outside="() => isOpen = !isOpen"
+          class="absolute top-0 right-5 bg-(--zaiko-bg-1) border border-(--zaiko-bg-2) rounded-md shadow-md w-fit min-w-36 z-10">
+          <ul class="flex flex-col gap-2">
+            <li class="p-2 hover:bg-(--zaiko-bg-2) hover:cursor-pointer" v-for="row in Object.keys(rows)"
+              @click="click(row)" :key="row">
+              {{ rows[row] }}
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
